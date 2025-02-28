ce86feb73a20c3d6ca6dd167a1d3366affe180dc
y23024119 <anisyaya44@gmail.com>
2025-02-28 17:24:38 +0100

    deux fonctions crees

--- src/ts/Sa2ts.java
+++ src/ts/Sa2ts.java
@@ -32,4 +32,85 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
 	//	System.out.println("</" + node.getClass().getSimpleName() + ">");
     }
 
+
+    public Void visit(SaDecVar node) throws Exception {
+        // Récupérer l'identifiant de la variable
+        String identif = node.getNom();
+
+
+
+
+        // Vérifier dans quel contexte on se trouve (global, local ou paramètre)
+        switch (context) {
+            case GLOBAL:
+                // Vérifier si la variable existe déjà dans la table globale
+                if (tableGlobale.getVar(identif) != null) {
+                    throw new Exception("Erreur: Variable " + identif + " déjà déclarée dans la portée globale.");
+                }
+                tableGlobale.addVar(identif, node.getType());
+                break;
+
+            case LOCAL:
+                // Vérifier si la variable existe déjà dans la table locale
+                if (tableLocaleCourante.getVar(identif) != null) {
+                    throw new Exception("Erreur: Variable " + identif + " déjà déclarée dans la portée locale.");
+                }
+                tableLocaleCourante.addVar(identif, node.getType());
+                break;
+
+            case PARAM:
+                // Vérifier si le paramètre est déjà déclaré
+                if (tableLocaleCourante.getVar(identif) != null) {
+                    throw new Exception("Erreur: Paramètre " + identif + " déjà déclaré dans la fonction.");
+                }
+                tableLocaleCourante.addParam(identif, node.getType());
+                break;
+        }
+
+        return null;
+    }
+    public Void visit(SaDecTab node) throws Exception {
+// Récupérer l'identifiant du tableau
+        String identif = node.getNom();
+        int taille = node.getTaille();
+        if (taille<0){
+            throw new Exception("Erreur: la taille doit etre positive.");
+        }
+
+
+        switch (context) {
+            case GLOBAL:
+                // Vérifier si la variable existe déjà dans la table globale
+                if (tableGlobale.getVar(identif) != null) {
+                    throw new Exception("Erreur: Variable " + identif + " déjà déclarée dans la portée globale.");
+                }
+                tableGlobale.addTab(identif,node.getType(), taille);
+                break;
+
+            case LOCAL:
+                // Vérifier si la variable existe déjà dans la table locale
+                if (tableLocaleCourante.getVar(identif) != null) {
+                    throw new Exception("Erreur: Variable " + identif + " déjà déclarée dans la portée locale.");
+                }
+                tableLocaleCourante.addTab(identif,node.getType(), taille);
+                break;
+
+            case PARAM:
+                // Vérifier si le paramètre est déjà déclaré
+                if (tableLocaleCourante.getVar(identif) != null) {
+                    throw new Exception("Erreur: Paramètre " + identif + " déjà déclaré dans la fonction.");
+                }
+                tableLocaleCourante.addTab(identif,node.getType(), taille);
+                break;
+        }
+
+        return null;
+
+    }
+
+
+
+
 }
+
+
