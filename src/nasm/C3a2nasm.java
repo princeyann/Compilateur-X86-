34e61ffb8faa5fe3714dfd1567fdf0d4346828e8
princeyann <yanis.liassine@etu.univ-amu.fr>
2025-03-28 12:31:35 +0100

    implementation de la méthode C3aInstMult C3aInstSub C3aInstAdd

--- src/nasm/C3a2nasm.java
+++ src/nasm/C3a2nasm.java
@@ -33,14 +33,41 @@ public class C3a2nasm implements C3aVisitor <NasmOperand> {
     }
     
     public NasmOperand visit(C3a c3a){return null;}
-    public NasmOperand visit(C3aInstAdd inst){return null;}
+
+    public NasmOperand visit(C3aInstAdd inst) {
+        NasmOperand label = (inst.label != null) ? inst.label.accept(this) : null;
+        NasmOperand oper1 = inst.op1.accept(this);
+        NasmOperand oper2 = inst.op2.accept(this);
+        NasmOperand dest = inst.result.accept(this);
+        nasm.ajouteInst(new NasmMov(label, dest, oper1, ""));
+        nasm.ajouteInst(new NasmAdd(null, dest, oper2, ""));
+        return null;
+    }
     public NasmOperand visit(C3aInstCall inst){return null;}
+
     public NasmOperand visit(C3aInstFBegin inst){return null;}
     public NasmOperand visit(C3aInst inst){return null;}
     public NasmOperand visit(C3aInstJumpIfLess inst){return null;}
-    public NasmOperand visit(C3aInstMult inst){return null;}
+
+    public NasmOperand visit(C3aInstMult inst){
+        NasmOperand label = (inst.label != null) ? inst.label.accept(this) : null;
+        NasmOperand op1 = inst.op1.accept(this);
+        NasmOperand op2 = inst.op2.accept(this);
+        NasmOperand dest = inst.result.accept(this);
+        nasm.ajouteInst(new NasmMov(label, dest, op1, ""));
+        nasm.ajouteInst(new NasmMul(null, dest, op2, ""));
+        return null;
+    }
     public NasmOperand visit(C3aInstRead inst){return null;}
-    public NasmOperand visit(C3aInstSub inst){return null;}
+    public NasmOperand visit(C3aInstSub inst){
+        NasmOperand label = (inst.label != null) ? inst.label.accept(this) : null;
+        NasmOperand oper1 = inst.op1.accept(this);
+        NasmOperand oper2 = inst.op2.accept(this);
+        NasmOperand dest = inst.result.accept(this);
+        nasm.ajouteInst(new NasmMov(label, dest, oper1, ""));
+        nasm.ajouteInst(new NasmSub(null, dest, oper2, ""));
+        return null;
+    }
     public NasmOperand visit(C3aInstAffect inst){return null;}
     public NasmOperand visit(C3aInstDiv inst){return null;}
     public NasmOperand visit(C3aInstFEnd inst){return null;}
